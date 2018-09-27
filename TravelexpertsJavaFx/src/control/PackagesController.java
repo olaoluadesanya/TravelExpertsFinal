f0d7d09947a516c54fc33b9fec3e99a1a808a862
Sunghyun Lee <773646@ACDM.DS.SAIT.CA>
2018-09-26 20:57:00 -0600

    javafx
    For now, in order to test, I set the Packages.fxml to be the main page. We will change this later.

---
+++ TravelexpertsJavaFx/src/control/PackagesController.java
@@ -0,0 +1,18 @@
+package control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PackagesController {
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private Label lblFName;
+
+}
