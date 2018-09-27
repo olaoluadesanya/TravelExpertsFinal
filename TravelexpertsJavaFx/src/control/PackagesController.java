462453436b63ff9448d0f644318c37f19b69f5b5
Sunghyun Lee <773646@ACDM.DS.SAIT.CA>
2018-09-26 21:15:57 -0600

    created PackagesDetail.fxm

--- TravelexpertsJavaFx/src/control/PackagesController.java
+++ TravelexpertsJavaFx/src/control/PackagesController.java
@@ -3,11 +3,15 @@ package control;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
 public class PackagesController {
 
     @FXML
     private Button btnSearch;
+
+    @FXML
+    private TableView<?> tvPackages;
 
     @FXML
     private Label lblId;
