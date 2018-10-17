550981ea06368094d68dfbb55c7a054e56e94111
sungcalgary <39603775+sungcalgary@users.noreply.github.com>
2018-10-17 10:27:07 -0600

    22

--- Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/MainActivity.java
+++ Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends Activity {
                 Packag packag = upcomingPackages.get(position);
                 Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
                 intent.putExtra("packag", packag);
+                intent.putExtra("customer", customer);
                 startActivity(intent);
             }
         });
