ffb72a1699d41920022887deaf2c43e2f50d4454
Olaoluwa Adesanya <790232@ACDM.DS.SAIT.CA>
2018-10-16 20:11:19 -0600

    fixed master branch

--- Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/LoginActivity.java
+++ Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/LoginActivity.java
@@ -50,6 +50,9 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import com.google.gson.reflect.TypeToken;
@@ -343,6 +346,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
         @Override
         protected Boolean doInBackground(Void... params) {
+
+
 
             JsonObject json = new JsonObject();
             json.addProperty("userid", mEmail);
--- Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/MainActivity.java
+++ Android/TravelExpertsAndroid/app/src/main/java/com/example/a790232/travelexpertsandroid/MainActivity.java
@@ -193,4 +193,3 @@ public class MainActivity extends Activity {
         }
     }
 }
-
